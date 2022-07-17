9fb5144fe666aa0f2276a1b02404d05befd5ffe4
mercylynn <mercy.kamene@student.moringaschool.com>
2022-07-17 18:23:50 +0300

    modify variables

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 
 function totalProduction(){
-    var a=parseInt(document.getElementById("txta").value);
-    var b=parseInt(document.getElementById("txtb").value);
-    var c=parseInt(document.getElementById("txtc").value);
-    var d=parseInt(document.getElementById("txtd").value);
-    var e=a+b+c+d;
-    document.getElementById("txte").value=""+e;
+    var a=parseInt(document.getElementById("sheda").value);
+    var b=parseInt(document.getElementById("shedb").value);
+    var c=parseInt(document.getElementById("shedc").value);
+    var d=parseInt(document.getElementById("shedd").value);
+    var totalLitres=a+b+c+d;
+    document.getElementById("totallitres").value=""+totalLitres;
+}
+function incomeOverTime(sellingPrice, time){
+    var perWeek = sellingPrice * time;
+    var perMonth = perWeek * 4;
+    var perYear = perMonth * 12;
+    document.getElementById("perWeek").value=""+perWeek;
+    document.getElementById("perMonth").value=""+perMonth;
+    document.getElementById("perYear").value=""+perYear;
 }
 
-var sellingPrice = document.getElementById("price");
-var time= document.getElementById("time");
-        
-function incomeOverTime(sellingPrice, time){
-/*
-    var f=parseInt(document.getElementById("txtf").value);
-    var g=f*e*7;
-    var h=f*e*7*12;
-    if(f>7){
-        document.getElementById("textg").value=""+g;  
-    }
-    else{
-        document.getElementById("texth").value=""+h;
-    }*/
-    
-   
-}
+
+// get values from the form when the user clicks the submit button
+document.getElementById('incomeForm').addEventListener("submit", e => {
+    e.preventDefault();
+
+    // get the values from the form
+    const buyingPrice = document.getElementById('buyingPrice').value;
+    const time = document.getElementById('time').value;
+    const litres = document.getElementById('litres').value;
+
+    // calculate the income
+    const sellingPrice = litres * buyingPrice;
+
+    incomeOverTime(sellingPrice, time);
+});
+
+
 
 
